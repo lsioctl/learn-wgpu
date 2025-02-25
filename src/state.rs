68a2d9c097b4498922ee2ff45fc2527431a4d28c
lsioctl <laurent.steff@gmail.com>
2025-02-25 16:47:23 -0400

    feat: texture, wip, but need to update wgpu

--- src/state.rs
+++ src/state.rs
@@ -101,6 +101,61 @@ impl State {
         };
         surface.configure(&device, &config);
 
+        let diffuse_bytes = include_bytes!("textures/happy-tree.png");
+        let diffuse_image = image::load_from_memory(diffuse_bytes).unwrap();
+        let diffuse_rgba = diffuse_image.to_rgba8();
+
+        use image::GenericImageView;
+        let dimensions = diffuse_image.dimensions();
+
+        let texture_size = wgpu::Extent3d {
+            width: dimensions.0,
+            height: dimensions.1,
+            // All textures are stored as 3D, we represent our 2D texture
+            // by setting depth to 1.
+            depth_or_array_layers: 1,
+        };
+        let diffuse_texture = device.create_texture(&wgpu::TextureDescriptor {
+            size: texture_size,
+            mip_level_count: 1, // We'll talk about this a little later
+            sample_count: 1,
+            dimension: wgpu::TextureDimension::D2,
+            // Most images are stored using sRGB, so we need to reflect that here.
+            format: wgpu::TextureFormat::Rgba8UnormSrgb,
+            // TEXTURE_BINDING tells wgpu that we want to use this texture in shaders
+            // COPY_DST means that we want to copy data to this texture
+            usage: wgpu::TextureUsages::TEXTURE_BINDING | wgpu::TextureUsages::COPY_DST,
+            label: Some("diffuse_texture"),
+            // This is the same as with the SurfaceConfig. It
+            // specifies what texture formats can be used to
+            // create TextureViews for this texture. The base
+            // texture format (Rgba8UnormSrgb in this case) is
+            // always supported. Note that using a different
+            // texture format is not supported on the WebGL2
+            // backend.
+            view_formats: &[],
+        });
+
+        // The Texture struct has no methods to interact with the data directly
+        // queue.write_texture(
+        //     // Tells wgpu where to copy the pixel data
+        //     wgpu::TexelCopyTextureInfo {
+        //         texture: &diffuse_texture,
+        //         mip_level: 0,
+        //         origin: wgpu::Origin3d::ZERO,
+        //         aspect: wgpu::TextureAspect::All,
+        //     },
+        //     // The actual pixel data
+        //     &diffuse_rgba,
+        //     // The layout of the texture
+        //     wgpu::TexelCopyBufferLayout {
+        //         offset: 0,
+        //         bytes_per_row: Some(4 * dimensions.0),
+        //         rows_per_image: Some(dimensions.1),
+        //     },
+        //     texture_size,
+        // );
+
         // a macro could also be used
         // let shader = device.create_shader_module(wgpu::include_wgsl!("shader.wgsl"));
         let shader_triangle = device.create_shader_module(wgpu::ShaderModuleDescriptor {
