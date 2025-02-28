61e648df8e0e1ce3ce5afb93206e454485331e8c
lsioctl <laurent.steff@gmail.com>
2025-02-27 22:32:54 -0400

    refacto: texture module ok for now

--- src/state.rs
+++ src/state.rs
@@ -102,96 +102,7 @@ impl<'a> State<'a> {
 
         surface.configure(&device, &config);
 
-        let texture = MyTexture::new(&device);
-
-        // The Texture struct has no methods to interact with the data directly
-        queue.write_texture(
-            // Tells wgpu where to copy the pixel data
-            wgpu::TexelCopyTextureInfo {
-                texture: &texture.diffuse_texture,
-                mip_level: 0,
-                origin: wgpu::Origin3d::ZERO,
-                aspect: wgpu::TextureAspect::All,
-            },
-            // The actual pixel data
-            &texture.diffuse_rgba,
-            // The layout of the texture
-            wgpu::TexelCopyBufferLayout {
-                offset: 0,
-                bytes_per_row: Some(4 * texture.dimensions.0),
-                rows_per_image: Some(texture.dimensions.1),
-            },
-            texture.texture_size,
-        );
-
-        // We don't need to configure the texture view much, so let's
-        // let wgpu define it.
-        let diffuse_texture_view = texture
-            .diffuse_texture
-            .create_view(&wgpu::TextureViewDescriptor::default());
-
-        let diffuse_sampler = device.create_sampler(&wgpu::SamplerDescriptor {
-            // what to do if the sample gets a texture coordinate
-            // which is out of the texture itself
-            address_mode_u: wgpu::AddressMode::ClampToEdge,
-            address_mode_v: wgpu::AddressMode::ClampToEdge,
-            address_mode_w: wgpu::AddressMode::ClampToEdge,
-            // what to do when the sample footprint is smaller or larger
-            // than one texel (usaually far from or close to the camera)
-            mag_filter: wgpu::FilterMode::Linear,
-            min_filter: wgpu::FilterMode::Nearest,
-            // mipmaps will be seen later
-            mipmap_filter: wgpu::FilterMode::Nearest,
-            ..Default::default()
-        });
-
-        // a bind group describes a set of ressources and how they are accessed by a shader
-        let texture_bind_group_layout =
-            device.create_bind_group_layout(&wgpu::BindGroupLayoutDescriptor {
-                entries: &[
-                    wgpu::BindGroupLayoutEntry {
-                        binding: 0,
-                        // only visible to the fs
-                        // possible values bitwise combinations
-                        // of NONE, VERTEX, FRAGMENT, COMPUTE
-                        visibility: wgpu::ShaderStages::FRAGMENT,
-                        ty: wgpu::BindingType::Texture {
-                            multisampled: false,
-                            view_dimension: wgpu::TextureViewDimension::D2,
-                            sample_type: wgpu::TextureSampleType::Float { filterable: true },
-                        },
-                        count: None,
-                    },
-                    wgpu::BindGroupLayoutEntry {
-                        binding: 1,
-                        // only visible to the fs
-                        visibility: wgpu::ShaderStages::FRAGMENT,
-                        // This should match the filterable field of the
-                        // corresponding Texture entry above.
-                        ty: wgpu::BindingType::Sampler(wgpu::SamplerBindingType::Filtering),
-                        count: None,
-                    },
-                ],
-                label: Some("texture_bind_group_layout"),
-            });
-
-        // This may seem not very DRY
-        // BindGroup is a more specific declaration of the bind group layout
-        // this pattern allows us to swap BindGroups on the fly as long as they have the same layout
-        let diffuse_bind_group = device.create_bind_group(&wgpu::BindGroupDescriptor {
-            layout: &texture_bind_group_layout,
-            entries: &[
-                wgpu::BindGroupEntry {
-                    binding: 0,
-                    resource: wgpu::BindingResource::TextureView(&diffuse_texture_view),
-                },
-                wgpu::BindGroupEntry {
-                    binding: 1,
-                    resource: wgpu::BindingResource::Sampler(&diffuse_sampler),
-                },
-            ],
-            label: Some("diffuse_bind_group"),
-        });
+        let texture = MyTexture::new(&device, &queue);
 
         // a macro could also be used
         // let shader = device.create_shader_module(wgpu::include_wgsl!("shader.wgsl"));
@@ -205,7 +116,7 @@ impl<'a> State<'a> {
         let render_pipeline_layout =
             device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
                 label: Some("Render Pipeline Layout"),
-                bind_group_layouts: &[&texture_bind_group_layout],
+                bind_group_layouts: &[&texture.bind_group_layout],
                 push_constant_ranges: &[],
             });
 
@@ -364,7 +275,7 @@ impl<'a> State<'a> {
             vertex_buffer,
             index_buffer,
             num_indices,
-            diffuse_bind_group,
+            diffuse_bind_group: texture.diffuse_bind_group,
         }
     }
 
